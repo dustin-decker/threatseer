bd77e979e38903fae08df700c67107fac205e058
Dustin Decker <dustindecker@protonmail.com>
2018-01-28 11:52:18 -0600

    include sensor

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"flag"
+	"runtime"
+	"time"
 
+	log "github.com/sirupsen/logrus"
+
+	"github.com/capsule8/capsule8/pkg/sensor"
 	"github.com/dustin-decker/threatseer/internal/app/agent"
 )
 
 func main() {
+	flag.Set("alsologtostderr", "true")
 	flag.Parse()
+	log.SetFormatter(&log.JSONFormatter{})
 
 	srv := agent.NewAgentServer()
 
+	go sensor.Main()
+
+	time.Sleep(10 * time.Second)
+
 	go srv.L3missDetector()
 
+	go srv.Telemetry()
+
 	// keep running
-	for {
-	}
+	runtime.Goexit()
+
 }
--- internal/app/agent/init.go
+++ internal/app/agent/init.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"os"
+	"os/signal"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -11,17 +12,23 @@ type Server struct {
 	Hostname string
 	MacAddr  string
 	IP       string
+	Signals  chan os.Signal
 }
 
 // NewAgentServer populates initial state
 func NewAgentServer() *Server {
 	hostname, err := os.Hostname()
 	if err != nil {
-		log.Fatalf("Couldn't get hostname: %v", err)
+		log.Fatalf("couldn't get hostname: %v", err)
 	}
+
+	// Exit cleanly on Control-C
+	signals := make(chan os.Signal)
+	signal.Notify(signals, os.Interrupt)
 
 	srv := Server{
 		Hostname: hostname,
+		Signals:  signals,
 	}
 	return &srv
 }
